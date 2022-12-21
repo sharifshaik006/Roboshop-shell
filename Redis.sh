524a02ace7bab47b8a657a1f6d15daf3b8e71b6a
sharifshaik006 <sharifshaik006@gmail.com>
2022-12-21 11:28:16 +0000

    scripts for Roboshop-shell project cart, user, Reddis, MySQL,RabbitMQ, Shipping,payment

---
+++ Redis.sh
@@ -0,0 +1,12 @@
+
+
+
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+yum install redis-6.2.7 -y
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module enable redis:remi-6.2 -y
+yum install redis -y
+
+systemctl enable redis
+systemctl start redis

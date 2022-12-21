6975a8bf8dab6dc049840ab8ff517eb8156ef3f3
sharifshaik006 <sharifshaik006@gmail.com>
2022-12-21 15:37:24 +0000

    issues1| Added scrpts and modifications through Redirectories in shell

--- cart.sh
+++ cart.sh
@@ -1,22 +1,35 @@
+set -e
+
+echo setting NOdeJs repos
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>>/tmp/cart.log
+
+echo installing NodeJs
+yum install nodejs -y  &>>/tmp/cart.log
 
 
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y
+echo Adding Application user
+useradd roboshop  &>>/tmp/cart.log
 
+echo Downloading application content
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>/tmp/cart.log
+cd /home/roboshop  &>>/tmp/cart.log
 
-useradd roboshop
+echo cleaning application content
+rm -rf cart  &>>/tmp/cart.log
 
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
-cd /home/roboshop
-unzip /tmp/cart.zip
-mv cart-main cart
-cd cart
-npm install
+echo extract Application Archive
+unzip -o /tmp/cart.zip  &>>/tmp/cart.log
+mv cart-main cart  &>>/tmp/cart.log
+cd cart  &>>/tmp/cart.log
 
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
-systemctl daemon-reload
-systemctl start cart
-systemctl enable cart
+echo installing NodeJs Dependencies
+npm install  &>>/tmp/cart.log
+
+echo configuring SystemD service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service  &>>/tmp/cart.log
+systemctl daemon-reload  &>>/tmp/cart.log
+
+echo starting cart service
+systemctl start cart  &>>/tmp/cart.log
+systemctl enable cart  &>>/tmp/cart.log
